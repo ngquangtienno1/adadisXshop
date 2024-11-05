94e0325e4a226f60f86ba543fe4e19341e1d2c8b
anpc33 <ancqph51578@gmail.com>
2024-11-05 22:53:14 +0700

    view header

--- admin/views/layouts/siderbar.php
+++ admin/views/layouts/siderbar.php
@@ -56,23 +56,55 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarDanhMuc" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDanhMuc">
-                        <i class="ri-stack-line"></i> <span data-key="t-advance-ui">Danh mục sản phẩm</span>
+                        <i class="ri-stack-line"></i> <span data-key="t-advance-ui">Quản lý danh mục</span>
                     </a>
                     <div class="collapse menu-dropdown" id="sidebarDanhMuc">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a href="" class="nav-link" data-key="t-sweet-alerts">
-                                    Danh sách
+                                <a href="?act=danh-mucs" class="nav-link" data-key="t-sweet-alerts">
+                                   Xem danh sách danh mục
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="" class="nav-link" data-key="t-nestable-list">
-                                    Thêm mới
+                                <a href="?act=form-danh-muc" class="nav-link" data-key="t-nestable-list">
+                                    Tạo danh mục
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="?act=form-sua-danh-muc" class="nav-link" data-key="t-nestable-list">
+                                    Sửa danh mục
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#sidebarDanhMuc" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarDanhMuc">
+                        <i class="ri-stack-line"></i> <span data-key="t-advance-ui">Quản lý liên hệ</span>
+                    </a>
+                    <div class="collapse menu-dropdown" id="sidebarDanhMuc">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="?act=lien-he" class="nav-link" data-key="t-sweet-alerts">
+                                    Xem liên hệ
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="?act=form-add-lien-he" class="nav-link" data-key="t-nestable-list">
+                                    Tạo liên hệ
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="?act=form-update-lien-he" class="nav-link" data-key="t-nestable-list">
+                                    Sửa liên hệ
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
 
                 <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Bán hàng</span></li>
 
